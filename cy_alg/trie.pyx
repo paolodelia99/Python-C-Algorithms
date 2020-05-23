03b3210f6166413d06dc6e96a5c2ab1914881c54
Paolo D'Elia <paolo.delia99@gmail.com>
2020-05-23 21:00:15 +0200

    Add trie tests

--- cy_alg/trie.pyx
+++ cy_alg/trie.pyx
@@ -13,16 +13,35 @@ cdef class Trie:
         if self._c_trie is not NULL:
             ctrie.trie_free(self._c_trie)
 
-    cpdef append(self,char* key,int value):
-        if not ctrie.trie_insert(self._c_trie, key, <void*><Py_ssize_t> value):
+    cdef char* get_c_string(self, str string):
+        """
+        Returns the C string of the given python string
+        
+        @param string the python string to transform in a c string
+        @return the C string of the given python string
+        """
+        byte_string = string.encode('UTF_8')
+        cdef char* c_string = byte_string
+        return c_string
+
+    cpdef append(self,str key,int value):
+
+        cdef char* c_key = self.get_c_string(key)
+
+        if not ctrie.trie_insert(self._c_trie, c_key, <void*><Py_ssize_t> value):
             raise MemoryError()
 
-    def remove(self,char* key):
-        if not ctrie.trie_remove(self._c_trie, key):
+    def remove(self,str key):
+        cdef char* c_key = self.get_c_string(key)
+
+        if not ctrie.trie_remove(self._c_trie, c_key):
             raise IndexError("Key is not present in the trie")
 
-    cpdef int lookup(self, char* key) except? -1:
-        cdef int value = <Py_ssize_t> ctrie.trie_lookup(self._c_trie, key)
+    cpdef int lookup(self, str key) except? -1:
+
+        cdef char* c_key = self.get_c_string(key)
+
+        cdef int value = <Py_ssize_t> ctrie.trie_lookup(self._c_trie, c_key)
 
         if value == 0:
 
--- test/test_trie.py
+++ test/test_trie.py
@@ -3,10 +3,46 @@ import trie
 
 Trie = trie.Trie
 
+
 def test_trie_101():
     t = Trie()
     nose.tools.assert_is_instance(t, trie.Trie)
 
+
 def test_trie_num_entries():
     t = Trie()
-    nose.tools.assert_equal(t.num_entries, 0)
+    nose.tools.assert_equal(t.num_entries(), 0)
+
+
+def test_trie_append():
+    t = Trie()
+    t.append("ci", 2)
+    nose.tools.assert_equal(t.num_entries(), 1)
+
+
+def test_trie_append_102():
+    t = Trie()
+    t.append("a", 40)
+    t.append("ab", 12)
+    t.append("abc", 14)
+    nose.tools.assert_equal(t.num_entries(), 3)
+
+
+def test_trie_remove():
+    t = Trie()
+    t.append("ab", 34)
+    t.append("a", 12)
+    t.remove("ab")
+    nose.tools.assert_equal(t.num_entries(), 1)
+
+
+def test_trie_lookup():
+    t = Trie()
+    t.append("a", 40)
+    t.append("ab", 12)
+    t.append("abc", 14)
+    t.append("b", 13)
+    nose.tools.assert_equal(t.lookup("a"), 40)
+    nose.tools.assert_equal(t.lookup("b"), 13)
+    nose.tools.assert_equal(t.lookup("ab"), 12)
+    nose.tools.assert_equal(t.lookup("abc"), 14)
